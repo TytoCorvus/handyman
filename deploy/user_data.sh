7998bd9de0974d89c3a67ea109adb2cabf05f447
TytoCorvus <brycecollins0@gmail.com>
2023-10-05 15:58:27 -0600

    Disabling actions for the time being

--- deploy/user_data.sh
+++ deploy/user_data.sh
@@ -1,1 +1,16 @@
 echo "User data script executed"
+
+NODE_MAJOR=18
+
+# Nodejs installation
+# This method courtesy of Nodesource repo: https://github.com/nodesource/distributions#ubuntu-versions
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt update
+sudo apt install nodejs -y
+
