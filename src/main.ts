2ee1a5b18da01862064e0ee9997a43635ab00033
TytoCorvus <brycecollins0@gmail.com>
2023-10-06 02:33:44 -0600

    Changing pattern

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`Environment: ${process.env.NODE_ENV}`);
   if(process.env.NODE_ENV === 'production') {
-    app.enableCors({origin: '*.brycecollins.net'})
+    app.enableCors({origin: new RegExp('.*\.brycecollins\.net')})
   }
   await app.listen(3000);
 }
