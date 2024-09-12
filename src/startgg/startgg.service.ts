b30902ebd09500c03357e486ace90412603f4493
TytoCorvus <brycecollins0@gmail.com>
2024-09-12 13:06:06 -0700

    WIP

--- src/startgg/startgg.service.ts
+++ src/startgg/startgg.service.ts
@@ -24,12 +24,12 @@ export class StartggService {
         }
         console.log(`Auth: ${headers.Authorization}`)
 
-        var query = this.buildTestQuery();
+        var query = this.buildTournamentsByNameQuery();
         return lastValueFrom(this.httpService.post(
             "https://api.start.gg/gql/alpha", 
             query, 
             { headers: headers }).pipe(tap(
-                response => console.log(response.data),
+                response => console.log(`Data: ${JSON.stringify(response.data)}`),
                 error => console.log(error)
             ), map(response => response.data)));
     }
@@ -41,6 +41,15 @@ export class StartggService {
         }
     }
 
+    buildUserQuery(): Object {
+        return {
+            "query": "query userQuery($slug:String){user(slug:$slug){id player{id gamerTag prefix}}}",
+            "variables": {
+                "slug": "user/a72d6f39"
+            }
+        }
+    }
+
     buildTestQuery(): Object {
         return {
             "query": "query getEventId($slug:String){event(slug:$slug){id name}}",
@@ -49,4 +58,35 @@ export class StartggService {
             }
         }
     }
+
+    buildTournamentQuery(): Object {
+        return {
+            "query": "query randomQueryName($slug:String){event(slug:$slug){id name}}",
+            "variables": {
+                "slug": "tournament/2xko-reno-rumble/event/2xko-double-elim"
+            }
+        }
+    }
+// 1685606 Tyto user
+
+    buildTournamentsByOwnerQuery(): Object {
+        return {
+            "query": "query TournamentsByOwner($perPage:Int!,$ownerId:ID!){tournaments(query:{perPage:$perPage filter:{ownerId:$ownerId}}){nodes{id name publishing startAt slug}}}",
+            "variables": {
+                "ownerId": 1685606,
+                "perPage": 100
+            }
+        }
+    }
+
+    buildTournamentsByNameQuery(): Object {
+        return {
+            "query": "query tournamentsByName($perPage:Int!,$name:String){tournaments(query:{perPage:$perPage filter:{name:$name}}) {nodes{id name publishing startAt slug}}}",
+            "variables": {
+                "name": "Tyto Tuesdays",
+                "perPage": 100
+            }
+        }
+    }
+
 }
